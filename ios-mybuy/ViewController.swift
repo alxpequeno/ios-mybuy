e8b1493b362246602c341117611c1cc32379170a
Alexis Pequeño <alxpequeno@gmail.com>
2018-11-21 09:36:16 -0500

    first commit

---
+++ ios-mybuy/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ios-mybuy
+//
+//  Created by Alexis on 21/11/18.
+//  Copyright © 2018 Alexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var txtNumero: UITextField!
+    @IBOutlet weak var txtContrasena: UITextField!
+    
+    
+    
+    @IBAction func btnIngresar(_ sender: Any) {
+        print("btnIngresar")
+    }
+    
+    
+    @IBAction func btnRegistrarse(_ sender: Any) {
+        print("btnRegistrarse")
+    }
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
