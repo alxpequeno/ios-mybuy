7df83f786a9c73efb8d7e4e854261d1e20188921
Alexis Pequeño <alxpequeno@gmail.com>
2018-11-23 16:41:02 -0500

    registro usuarios servicio

--- ios-mybuy/RegistrarseViewController.swift
+++ ios-mybuy/RegistrarseViewController.swift
@@ -7,26 +7,59 @@
 //
 
 import UIKit
+import Alamofire
 
 class RegistrarseViewController: UIViewController {
 
+    
+    @IBOutlet weak var txtNumero: UITextField!
+    @IBOutlet weak var txtContrasena: UITextField!
+    @IBOutlet weak var txtNombre: UITextField!
+    @IBOutlet weak var txtCategoria: UITextField!
+    @IBOutlet weak var txtDescripcion: UITextField!
+    
+    
+    @IBAction func btAceptar(_ sender: Any) {
+        guard let numero = txtNumero.text else {
+            return
+        }
+        
+        guard let contrasena = txtContrasena.text else {
+            return
+        }
+        
+        guard let nombre = txtNombre.text else {
+            return
+        }
+        
+        guard let categoria = txtCategoria.text else {
+            return
+        }
+        
+        guard let descripcion = txtDescripcion.text else {
+            return
+        }
+        
+        let parametros = [
+            "numero_telefono": numero,
+            "contrasenia": contrasena,
+            "nickname": nombre,
+            "categoria": categoria,
+            "numero_cuenta_bancaria": "foo",
+            "descripcion": descripcion
+        ]
+        
+        let URL_REGISTRO = "http://localhost:8080/MyBuy/rest/MyBuyRest/registrarUsuario"
+        
+        Alamofire.request(URL_REGISTRO,method: .post,parameters: parametros, encoding: JSONEncoding.default).responseJSON { response in
+            
+            print(response)
+        }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
